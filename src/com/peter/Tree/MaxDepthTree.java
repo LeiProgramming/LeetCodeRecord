38d2bdb94f331851ee31087306667b4c6d04df3b
Peter <18681694607@163.com>
2022-04-13 15:14:14 +0800

    104. 二叉树的最大深度

---
+++ src/com/peter/Tree/MaxDepthTree.java
@@ -0,0 +1,28 @@
+package com.peter.Tree;
+
+public class MaxDepthTree {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int leftDepth = maxDepth(root.left);
+        int rightDepth = maxDepth(root.right);
+        int max = 1 + Math.max(leftDepth, rightDepth);
+        return max;
+    }
+
+    private class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode() {
+        }
+
+        public TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
